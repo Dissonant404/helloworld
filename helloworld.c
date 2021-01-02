a525713d00be668a0afa9ff7dd5bfc22debdfde8
sagnikroy2005 <66377980+sagnikroy2005@users.noreply.github.com>
2021-01-02 20:08:31 +0600

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void main()
+{
+	char h[10]="hello",e[10]="world",i,j;
+	for(i=0;h[i]!='\0';++i);
+	//printf("%d" "%s",i,h[i]);
+	
+	h[i]=' ';
+	//printf("%d" "%c",i,h[i]);
+	
+	i=i+1;
+	//printf("%d" "%c",i,h[i]);
+	
+	for(j=0; e[j]!='\0'; ++j, ++i)
+	{
+		h[i]=e[j];
+	}
+	printf("%s",h);
+
+	printf("%c",x);
+	int i;
+	for (i=0;i<=127;i++)
+	
+	{
+		i=(char)i;
+		printf("\n %c",i);
+	}
+}
+
