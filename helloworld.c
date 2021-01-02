6ae492ed752ff8b68591535be7d415575e42fedc
sagnikroy2005 <66377980+sagnikroy2005@users.noreply.github.com>
2021-01-02 20:12:47 +0600

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -4,27 +4,14 @@ void main()
 {
 	char h[10]="Hello",e[10]="World",i,j;
 	for(i=0;h[i]!='\0';++i);
-	//printf("%d" "%s",i,h[i]);
 	
 	h[i]=' ';
-	//printf("%d" "%c",i,h[i]);
-	
 	i=i+1;
-	//printf("%d" "%c",i,h[i]);
 	
 	for(j=0; e[j]!='\0'; ++j, ++i)
 	{
 		h[i]=e[j];
 	}
 	printf("%s",h);
-
-	printf("%c",x);
-	int i;
-	for (i=0;i<=127;i++)
-	
-	{
-		i=(char)i;
-		printf("\n %c",i);
-	}
 }
 
