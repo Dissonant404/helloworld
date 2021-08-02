c0dbee8651c25bec8fc3d7c69db8f9f73e42e445
SRoy404 <66377980+SRoy404@users.noreply.github.com>
2021-08-02 19:28:42 +0600

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
-
-void main()
-{
-	char h[10]="Hello",e[10]="World",i,j;
-	for(i=0;h[i]!='\0';++i);
-	
-	h[i]=' ';
-	i=i+1;
-	
-	for(j=0; e[j]!='\0'; ++j, ++i)
-	{
-		h[i]=e[j];
-	}
-	printf("%s",h);
-}
 
